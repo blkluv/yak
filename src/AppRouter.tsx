eb20db664d03b7cea64a74841e6716d31fe6e960
WEB5D <info@blkluv.org>
2026-03-03 19:36:39 -0500

    Update AppRouter.tsx

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { SimpleAudioTest } from '@/components/SimpleAudioTest'; // ← ADD THIS IMPORT
 
 export function AppRouter() {
   return <RouterProvider router={router} />;
